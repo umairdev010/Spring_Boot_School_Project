4cd32de6852f55f0b971c85b9d214ef3f84aec84
Umair Tariq <umairdev010@gmail.com>
2026-02-06 17:56:54 +0500

    creating new project on friday

---
+++ src/main/java/org/practice/umair/spring_boot_school_project/SpringBootSchoolProjectApplication.java
@@ -0,0 +1,14 @@
+package org.practice.umair.spring_boot_school_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchoolProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSchoolProjectApplication.class, args);
+        System.out.println("Wellcome to spring.");
+    }
+
+}
---
+++ src/test/java/org/practice/umair/spring_boot_school_project/SpringBootSchoolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.umair.spring_boot_school_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSchoolProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
